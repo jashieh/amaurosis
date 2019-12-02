6de4de851a8f9bc9e83075a825ade86daea1af82
justin <shieh_justin@yahoo.com>
2019-12-01 21:02:04 -0800

    change location of index.html for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 module.exports = {
+  context: __dirname,
   entry: './lib/index.js',
   output: {
     filename: 'main.js'
+  },
+  resolve: {
+    extensions: [".js"]
   },
   devtool: 'source-map'
 }
